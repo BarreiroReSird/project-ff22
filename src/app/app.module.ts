ec83f81317bdd1be57f9c4f38a3a9b8de5c5d53c
BarreiroReSird <carlosbarreiro2604@gmail.com>
2022-04-05 20:56:59 +0100

    Creating the drivers table page and starting it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,8 @@ import { HelloComponent } from './hello.component';
 import { RouterModule } from '@angular/router';
 import { HomeComponent } from '../home/home.component';
 import { StandingsComponent } from '../standings/standings.component';
-import { PageNotFoundComponent } from '../page-not-found/page-not-found.component'
+import { PageNotFoundComponent } from '../page-not-found/page-not-found.component';
+import { DriverStandingsComponent } from '../driver-standings/driver-standings.component';
 
 @NgModule({
   imports: [
@@ -16,8 +17,9 @@ import { PageNotFoundComponent } from '../page-not-found/page-not-found.componen
     RouterModule.forRoot([
       { path: '', component: HomeComponent },
       { path: 'team-standings', component: StandingsComponent },
-      {path: '404', component: PageNotFoundComponent},
-      {path: '**', redirectTo: '/404'}
+      { path: 'driver-standings', component: DriverStandingsComponent },
+      { path: '404', component: PageNotFoundComponent },
+      { path: '**', redirectTo: '/404' },
     ]),
   ],
   declarations: [AppComponent, HelloComponent],

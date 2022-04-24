54388c3407838881529bbd1c4ff709ade58aa75a
BarreiroReSird <carlosbarreiro2604@gmail.com>
2022-04-25 00:08:56 +0100

    New interface, under development, for the constructors' standings page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HomeComponent } from '../home/home.component';
 import { StandingsComponent } from '../standings/standings.component';
 import { PageNotFoundComponent } from '../page-not-found/page-not-found.component';
 import { DriverStandingsComponent } from '../driver-standings/driver-standings.component';
+import { ConstructorStandingsComponent } from '../constructor-standings/constructor-standings.component';
 
 @NgModule({
   imports: [
@@ -16,7 +17,8 @@ import { DriverStandingsComponent } from '../driver-standings/driver-standings.c
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent },
-      { path: 'team-standings', component: StandingsComponent },
+      // { path: 'team-standings', component: StandingsComponent },
+      { path: 'team-standings', component: ConstructorStandingsComponent },
       { path: 'driver-standings', component: DriverStandingsComponent },
       { path: '404', component: PageNotFoundComponent },
       { path: '**', redirectTo: '/404' },
---
+++ src/constructor-standings/constructor-standings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-constructor-standings',
+  templateUrl: './constructor-standings.component.html',
+  styleUrls: ['./constructor-standings.component.css']
+})
+export class ConstructorStandingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
